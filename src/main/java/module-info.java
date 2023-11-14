a6ed811b310a4924cb147fa157497f2bf47653ed
vagosu <victorss_26@hotmail.com>
2023-11-14 19:33:33 +0100

    Inicio

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ud04poot01 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ud04poot01 to javafx.fxml;
-    exports com.example.ud04poot01;
-}
